07396c1a84eb921dbe66d82ac2e53664cd698398
Plamen Balkanski <plamen@leavewizard.com>
2025-01-09 11:14:11 +0000

    Add React app base files

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TimeClock from './components/TimeClock';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <TimeClock employeeId={1} />
+  </React.StrictMode>
+); 
